1128c7cd1eac09d3297ec774cf9192882dc7fadd
EduardGetman <59934976+EduardGetman@users.noreply.github.com>
2021-02-19 15:34:00 +0300

    Создал ViewModel и MainWindowViewModel

--- FunctionVisualizer/ViewModels/Base/ViewModel.cs
+++ FunctionVisualizer/ViewModels/Base/ViewModel.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace FunctionVisualizer.ViewModels.Base
 {
-    abstract class ViewModel
+    abstract class ViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string ProperyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(ProperyName));
+        }
+        protected virtual bool Set<T>(ref T fieled, T value, [CallerMemberName] string ProperyName = null)
+        {
+            if (Equals(fieled, value))
+                return false;
+            OnPropertyChanged(ProperyName);
+            return true;
+        }
     }
 }
---
+++ FunctionVisualizer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FunctionVisualizer.ViewModels.Base;
+
+namespace FunctionVisualizer.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Визуализатор математических функций";
+        public string Title 
+        {
+            get => _Title;
+            set => Set(ref _Title, value);     
+        }
+    }
+}
